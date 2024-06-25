e0af3df53925fb16d68bce5ce9422a7a1edd4129
User-164 <lukliu990@gmail.com>
2024-06-25 15:23:39 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,74 +1,16 @@
-let thing = document.getElementById("d");
-thing.addEventListener("click", function() {
-document.body.style.backgroundColor = "green"
+const express = require('express');
+const app = express();
+const port = 3000;
+
+  app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/home.html');
+  });
+
+
+app.get('/about', (req,res) => {
+  res.sendFile(__dirname + '/bio.html');
 });
 
-let thing1 = document.getElementById("id");
-thing1.addEventListener("click", function() {
-document.body.style.backgroundColor = "red"
-});
-
-let thing2 = document.getElementById("iid");
-thing2.addEventListener("click", function() {
-document.body.style.backgroundColor = "red"
-});
-
-let thing3 = document.getElementById("iiid");
-thing3.addEventListener("click", function() {
-document.body.style.backgroundColor = "red"
-});
-
-let thing4 = document.getElementById("iiiid");
-thing4.addEventListener("click", function() {
-document.body.style.backgroundColor = "red"
-});
-
-let thing5 = document.getElementById("iiiiid");
-thing5.addEventListener("click", function() {
-document.body.style.backgroundColor = "green"
-});
-
-let thing6 = document.getElementById("iiiiiid");
-thing6.addEventListener("click", function() {
-document.body.style.backgroundColor = "red"
-});
-
-let thing7 = document.getElementById("iiiiiiid");
-thing7.addEventListener("click", function() {
-document.body.style.backgroundColor = "red"
-});
-
-let thing8 = document.getElementById("iiiiiiiid");
-thing8.addEventListener("click", function() {
-document.body.style.backgroundColor = "red"
-});
-
-let thing9 = document.getElementById("iiiiiiiiid");
-thing9.addEventListener("click", function() {
-document.body.style.backgroundColor = "red"
-});
-
-let thing10 = document.getElementById("iiiiiiiiiid");
-thing10.addEventListener("click", function() {
-document.body.style.backgroundColor = "green"
-});
-
-let thing11 = document.getElementById("iiiiiiiiiiid");
-thing11.addEventListener("click", function() {
-document.body.style.backgroundColor = "red"
-});
-
-let thing12 = document.getElementById("iiiiiiiiiiiid");
-thing12.addEventListener("click", function() {
-document.body.style.backgroundColor = "green"
-});
-
-let thing13 = document.getElementById("iiiiiiiiiiiiid");
-thing13.addEventListener("click", function() {
-document.body.style.backgroundColor = "red"
-});
-
-let thing14 = document.getElementById("iiiiiiiiiiiid");
-thing14.addEventListener("click", function() {
-document.body.style.backgroundColor = "red"
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
 });
